b08be02488aaad9df7d4d1e10c1125412862c24b
LucasFernandezC <lucas.fernandez.carbonell@gmail.com>
2022-07-14 16:52:03 +0200

    creacion de componente del header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <p>Hello World</p>
       </header>
