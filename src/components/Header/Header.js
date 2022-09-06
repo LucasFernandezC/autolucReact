9473d20b43466387a7b59f6b874e26f74d7eede8
Lucasfernandezc <lucas.fernandez.carbonell@gmail.com>
2022-09-06 15:41:09 +0200

    problemas de branch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
-<header className="cabeza row">
+<header className="cabeza row ">
         <div className="col-lg-3 col-sm-12 float-start headimage">
             <Link to="/" className="animate__animated animate__zoomInDown"><img src="/assets/images/logo.png"
                     className="rounded" alt="Logo de la empresa" title="Logo"></img></Link>
