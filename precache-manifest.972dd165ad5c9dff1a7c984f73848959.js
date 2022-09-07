10ddf71cde1c968c596c61979bc2a6b8e367ba3a
Lucasfernandezc <lucas.fernandez.carbonell@gmail.com>
2022-09-07 13:23:27 +0200

    Updates

---
+++ precache-manifest.972dd165ad5c9dff1a7c984f73848959.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d9ffd7db919c49a537",
+    "url": "/autolucReact/static/css/main.65c9c671.chunk.css"
+  },
+  {
+    "revision": "58d9ffd7db919c49a537",
+    "url": "/autolucReact/static/js/main.58d9ffd7.chunk.js"
+  },
+  {
+    "revision": "68ca4d39d6a968f01ce9",
+    "url": "/autolucReact/static/css/1.ed88a9d7.chunk.css"
+  },
+  {
+    "revision": "68ca4d39d6a968f01ce9",
+    "url": "/autolucReact/static/js/1.68ca4d39.chunk.js"
+  },
+  {
+    "revision": "ae897b30b29dc7277bf2",
+    "url": "/autolucReact/static/js/runtime~main.ae897b30.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/autolucReact/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/autolucReact/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "d216011717404b25f498159662cb7cf0",
+    "url": "/autolucReact/index.html"
+  }
+];
